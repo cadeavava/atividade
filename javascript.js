c205467302c09ca6bc0a625302c5102c090b2a18
cadeavava <137566635+cadeavava@users.noreply.github.com>
2023-10-25 15:02:01 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu");
+    const nav = document.querySelector("nav");
+
+    menuIcon.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+        });
+    });
+});
+
+
+
+
+
